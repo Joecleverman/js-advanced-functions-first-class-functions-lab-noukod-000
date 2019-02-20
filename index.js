c4ab4fe8513ec50c872327bce08aeaccae75fcac
Josue Abraham <josuea@noukod.com>
2019-02-20 18:41:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 // Code your solution in this file!
- const returnFirstTwoDrivers = function(array) {
-    return array.slice(0, 2);
- };
+const returnFirstTwoDrivers = function (drivers) {
+   return drivers.slice(0,2);
+}
 
- const returnLastTwoDrivers = function(array) {
-     return array.slice(-2);
- };
+const returnLastTwoDrivers = function (drivers) {
+   return drivers.slice(-2);
+}
 
- const selectingDrivers = [
-     returnFirstTwoDrivers, returnLastTwoDrivers
- ];
+const selectingDrivers = [
+   returnFirstTwoDrivers,
+   returnLastTwoDrivers
+];
 
- const createFareMultiplier = function(int) {
-    return function(arg) {
-        return int * arg;
-   };
-};
- const fareDoubler = createFareMultiplier(2);
+function createFareMultiplier(number){
+   return function (fare){return number * fare};
+}
 
+const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
-function fetchSpecifiedDrivers(driverArray, fn) {
-     return fn(driverArray);
- }
+function selectDifferentDrivers(drivers, cb){
+   return cb(drivers);
+}
